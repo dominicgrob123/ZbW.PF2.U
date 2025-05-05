79d4815bd185c4548a1e24171203f41067ca0b53
Mesaros, Thomas <thomas.mesaros@unisg.ch>
2025-05-05 14:45:56 +0200

    StringHasher added

---
+++ MB14Test/StringHasherTest.cs
@@ -0,0 +1,49 @@
+﻿using MB14;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB14Test
+{
+  [TestClass]
+  public class StringHasherTest
+  {
+    [TestMethod]
+    public void Execute_All_For_Foo()
+    {
+      // arrange
+      var foo = "foo";
+
+      // act
+      var additive = StringHasher.AdditiveHash(foo);
+      var djb2 = StringHasher.Djb2(foo);
+      var folding = StringHasher.FoldingHash(foo);
+
+      // assert
+
+      Console.WriteLine($"Additive: {additive}");
+      Console.WriteLine($"Folding:  {folding}");
+      Console.WriteLine($"DJB2:     {djb2}");
+    }
+
+    [TestMethod]
+    public void ExecuteAll_For_Oof()
+    {
+      // arrange
+      var foo = "oof";
+
+      // act
+      var additive = StringHasher.AdditiveHash(foo);
+      var djb2 = StringHasher.Djb2(foo);
+      var folding = StringHasher.FoldingHash(foo);
+
+      // assert
+
+      Console.WriteLine($"Additive: {additive}");
+      Console.WriteLine($"Folding:  {folding}");
+      Console.WriteLine($"DJB2:     {djb2}");
+    }
+  }
+}
